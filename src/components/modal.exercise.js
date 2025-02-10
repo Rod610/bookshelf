e20dc0e3f090d8eeed625ee9d21aa82eb2884d8f
Rodrigo Martinez <rod610@outlook.com>
2025-02-10 16:41:37 -0500

    exercise 8 solution

--- src/components/modal.exercise.js
+++ src/components/modal.exercise.js
@@ -1,7 +1,8 @@
 // 🐨 you're going to need the Dialog component
 // It's just a light wrapper around ReachUI Dialog
 // 📜 https://reacttraining.com/reach-ui/dialog/
-// import {Dialog} from './lib'
+import VisuallyHidden from '@reach/visually-hidden'
+import {Dialog, CircleButton} from './lib'
 
 // 💰 Here's a reminder of how your components will be used:
 /*
@@ -19,14 +20,28 @@
 </Modal>
 */
 
+import * as React from 'react'
 // we need this set of compound components to be structurally flexible
 // meaning we don't have control over the structure of the components. But
 // we still want to have implicitly shared state, so...
 // 🐨 create a ModalContext here with React.createContext
 
+const callAll =
+  (...fns) =>
+  (...args) =>
+    fns.forEach(fn => fn && fn(...args))
+
+const ModalContext = React.createContext()
+
 // 🐨 create a Modal component that manages the isOpen state (via useState)
 // and renders the ModalContext.Provider with the value which will pass the
 // isOpen state and setIsOpen function
+
+const Modal = props => {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  return <ModalContext.Provider value={[isOpen, setIsOpen]} {...props} />
+}
 
 // 🐨 create a ModalDismissButton component that accepts children which will be
 // the button which we want to clone to set it's onClick prop to trigger the
@@ -35,11 +50,61 @@
 // 💰 to get the setIsOpen function you'll need, you'll have to useContext!
 // 💰 keep in mind that the children prop will be a single child (the user's button)
 
+const ModalDismissButton = ({children}) => {
+  const [, setIsOpen] = React.useContext(ModalContext)
+
+  return React.cloneElement(children, {
+    onClick: callAll(() => setIsOpen(false), children.props.onClick),
+  })
+}
+
 // 🐨 create a ModalOpenButton component which is effectively the same thing as
 // ModalDismissButton except the onClick sets isOpen to true
+
+const ModalOpenButton = ({children}) => {
+  const [, setIsOpen] = React.useContext(ModalContext)
+
+  return React.cloneElement(children, {
+    onClick: callAll(() => setIsOpen(true), children.props.onClick),
+  })
+}
 
 // 🐨 create a ModalContents component which renders the Dialog.
 // Set the isOpen prop and the onDismiss prop should set isOpen to close
 // 💰 be sure to forward along the rest of the props (especially children).
 
+// const ModalContents = props => {
+//   const [isOpen, setIsOpen] = React.useContext(ModalContext)
+
+//   return (
+//     <Dialog isOpen={isOpen} onDismiss={() => setIsOpen(false)} {...props} />
+//   )
+// }
+
+function ModalContentsBase(props) {
+  const [isOpen, setIsOpen] = React.useContext(ModalContext)
+  return (
+    <Dialog isOpen={isOpen} onDismiss={() => setIsOpen(false)} {...props} />
+  )
+}
+
+function ModalContents({title, children, ...props}) {
+  return (
+    <ModalContentsBase {...props}>
+      <div css={{display: 'flex', justifyContent: 'flex-end'}}>
+        <ModalDismissButton>
+          <CircleButton>
+            <VisuallyHidden>Close</VisuallyHidden>
+            <span aria-hidden>×</span>
+          </CircleButton>
+        </ModalDismissButton>
+      </div>
+      <h3 css={{textAlign: 'center', fontSize: '2em'}}>{title}</h3>
+      {children}
+    </ModalContentsBase>
+  )
+}
+
 // 🐨 don't forget to export all the components here
+
+export {Modal, ModalDismissButton, ModalOpenButton, ModalContents}
--- src/unauthenticated-app.exercise.js
+++ src/unauthenticated-app.exercise.js
@@ -2,19 +2,25 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-import VisuallyHidden from '@reach/visually-hidden'
+//import VisuallyHidden from '@reach/visually-hidden'
 import {
   Input,
-  CircleButton,
+  //CircleButton,
   Button,
   Spinner,
   FormGroup,
   ErrorMessage,
   // 💣 when you're all done, you won't need this Dialog anymore
   // you can remove this now or later when you've finished
-  Dialog,
+  //Dialog,
 } from './components/lib'
 // 🐨 import all the Modal compound components you created in ./components/modal
+import {
+  Modal,
+  //ModalDismissButton,
+  ModalOpenButton,
+  ModalContents,
+} from './components/modal'
 import {Logo} from './components/logo'
 import {useAuth} from './context/auth-context'
 import {useAsync} from './utils/hooks'
@@ -71,36 +77,47 @@ function LoginForm({onSubmit, submitButton}) {
 }
 
 // 💣 when you're all done, you'll be able to completely delete this
-function LoginFormModal({
-  onSubmit,
-  modalTitleText,
-  modalLabelText,
-  submitButton,
-  openButton,
-}) {
-  const [isOpen, setIsOpen] = React.useState(false)
+// function LoginFormModal({
+//   onSubmit,
+//   modalTitleText,
+//   modalLabelText,
+//   submitButton,
+//   openButton,
+// }) {
+//   const [isOpen, setIsOpen] = React.useState(false)
 
-  return (
-    <React.Fragment>
-      {React.cloneElement(openButton, {onClick: () => setIsOpen(true)})}
-      <Dialog
-        aria-label={modalLabelText}
-        isOpen={isOpen}
-        onDismiss={() => setIsOpen(false)}
-      >
-        <div css={{display: 'flex', justifyContent: 'flex-end'}}>
-          {/* 💰 here's what you should put in your <ModalDismissButton> */}
-          <CircleButton onClick={() => setIsOpen(false)}>
-            <VisuallyHidden>Close</VisuallyHidden>
-            <span aria-hidden>×</span>
-          </CircleButton>
-        </div>
-        <h3 css={{textAlign: 'center', fontSize: '2em'}}>{modalTitleText}</h3>
-        <LoginForm onSubmit={onSubmit} submitButton={submitButton} />
-      </Dialog>
-    </React.Fragment>
-  )
-}
+//   return (
+//     <React.Fragment>
+//       {React.cloneElement(openButton, {onClick: () => setIsOpen(true)})}
+//       <Dialog
+//         aria-label={modalLabelText}
+//         isOpen={isOpen}
+//         onDismiss={() => setIsOpen(false)}
+//       >
+//         <div css={{display: 'flex', justifyContent: 'flex-end'}}>
+//           {/* 💰 here's what you should put in your <ModalDismissButton> */}
+//           <CircleButton onClick={() => setIsOpen(false)}>
+//             <VisuallyHidden>Close</VisuallyHidden>
+//             <span aria-hidden>×</span>
+//           </CircleButton>
+//         </div>
+//         <h3 css={{textAlign: 'center', fontSize: '2em'}}>{modalTitleText}</h3>
+//         <LoginForm onSubmit={onSubmit} submitButton={submitButton} />
+//       </Dialog>
+//     </React.Fragment>
+//   )
+// }
+
+// const circleDismissButton = (
+//   <div css={{display: 'flex', justifyContent: 'flex-end'}}>
+//     <ModalDismissButton>
+//       <CircleButton>
+//         <VisuallyHidden>Close</VisuallyHidden>
+//         <span aria-hidden>×</span>
+//       </CircleButton>
+//     </ModalDismissButton>
+//   </div>
+// )
 
 function UnauthenticatedApp() {
   const {login, register} = useAuth()
@@ -130,7 +147,8 @@ function UnauthenticatedApp() {
              it did when you started, but the extra credits will help clean
              things up a bit.
         */}
-        <LoginFormModal
+
+        {/* <LoginFormModal
           onSubmit={login}
           modalTitleText="Login"
           modalLabelText="Login form"
@@ -143,7 +161,57 @@ function UnauthenticatedApp() {
           modalLabelText="Registration form"
           submitButton={<Button variant="secondary">Register</Button>}
           openButton={<Button variant="secondary">Register</Button>}
-        />
+        /> */}
+
+        {/* <Modal>
+          <ModalOpenButton>
+            <Button variant="primary">Login</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Login form">
+            {circleDismissButton}
+            <h3 css={{textAlign: 'center', fontSize: '2em'}}>Login</h3>
+            <LoginForm
+              onSubmit={login}
+              submitButton={<Button variant="primary">Login</Button>}
+            />
+          </ModalContents>
+        </Modal>
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="secondary">Register</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Registration form">
+            {circleDismissButton}
+            <h3 css={{textAlign: 'center', fontSize: '2em'}}>Register</h3>
+            <LoginForm
+              onSubmit={register}
+              submitButton={<Button variant="secondary">Register</Button>}
+            />
+          </ModalContents>
+        </Modal> */}
+
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="primary">Login</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Login form" title="Login">
+            <LoginForm
+              onSubmit={login}
+              submitButton={<Button variant="primary">Login</Button>}
+            />
+          </ModalContents>
+        </Modal>
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="secondary">Register</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Registration form" title="Register">
+            <LoginForm
+              onSubmit={register}
+              submitButton={<Button variant="secondary">Register</Button>}
+            />
+          </ModalContents>
+        </Modal>
       </div>
     </div>
   )
