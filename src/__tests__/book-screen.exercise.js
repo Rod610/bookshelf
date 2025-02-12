b66913f752468e89260fbff641b3a1ca6154edd8
Rodrigo Martinez <rod610@outlook.com>
2025-02-12 16:27:23 -0500

    exercise 13 solution - no extras

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,34 +1,97 @@
 // 🐨 here are the things you're going to need for this test:
-// import * as React from 'react'
-// import {render, screen, waitFor} from '@testing-library/react'
-// import {queryCache} from 'react-query'
-// import {buildUser, buildBook} from 'test/generate'
-// import * as auth from 'auth-provider'
-// import {AppProviders} from 'context'
-// import {App} from 'app'
+import * as React from 'react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {queryCache} from 'react-query'
+import {buildUser, buildBook} from 'test/generate'
+import * as auth from 'auth-provider'
+import {AppProviders} from 'context'
+import {App} from 'app'
 
 // 🐨 after each test, clear the queryCache and auth.logout
+// general cleanup
+afterEach(async () => {
+  queryCache.clear()
+  await auth.logout()
+})
 
-test.todo('renders all the book information')
-// 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
+test('renders all the book information', async () => {
+  // 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
+  const fakeToken = 'SOME_FAKE_TOKEN'
+  // 🐨 create a user using `buildUser`
+  const user = buildUser()
+  window.localStorage.setItem(auth.localStorageKey, fakeToken)
+  // 🐨 create a book use `buildBook`
+  const book = buildBook()
 
-// 🐨 create a user using `buildUser`
-// 🐨 create a book use `buildBook`
-// 🐨 update the URL to `/book/${book.id}`
-//   💰 window.history.pushState({}, 'page title', route)
-//   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
+  // 🐨 update the URL to `/book/${book.id}`
+  const route = `/book/${book.id}`
+  //   💰 window.history.pushState({}, 'page title', route)
+  //   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
 
-// 🐨 reassign window.fetch to another function and handle the following requests:
-// - url ends with `/bootstrap`: respond with {user, listItems: []}
-// - url ends with `/list-items`: respond with {listItems: []}
-// - url ends with `/books/${book.id}`: respond with {book}
-// 💰 window.fetch = async (url, config) => { /* handle stuff here*/ }
-// 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
+  window.history.pushState({}, 'Test page', route)
 
-// 🐨 render the App component and set the wrapper to the AppProviders
-// (that way, all the same providers we have in the app will be available in our tests)
+  // 🐨 reassign window.fetch to another function and handle the following requests:
+  // - url ends with `/bootstrap`: respond with {user, listItems: []}
+  // - url ends with `/list-items`: respond with {listItems: []}
+  // - url ends with `/books/${book.id}`: respond with {book}
+  // 💰 window.fetch = async (url, config) => { /* handle stuff here*/ }
+  // 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
 
-// 🐨 use findBy to wait for the book title to appear
-// 📜 https://testing-library.com/docs/dom-testing-library/api-async#findby-queries
+  const originalFetch = window.fetch
 
-// 🐨 assert the book's info is in the document
+  window.fetch = async (url, config) => {
+    if (url.endsWith('/bootstrap')) {
+      return {
+        ok: true,
+        json: async () => ({
+          user: {...user, token: fakeToken},
+          listItems: [],
+        }),
+      }
+    } else if (url.endsWith(`/books/${book.id}`)) {
+      return {ok: true, json: async () => ({book})}
+    }
+
+    return originalFetch(url, config)
+  }
+
+  // 🐨 render the App component and set the wrapper to the AppProviders
+  // (that way, all the same providers we have in the app will be available in our tests)
+
+  render(<App />, {wrapper: AppProviders})
+
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
+
+  // 🐨 use findBy to wait for the book title to appear
+  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#findby-queries
+
+  // 🐨 assert the book's info is in the document
+
+   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+    expect(screen.getByText(book.author)).toBeInTheDocument()
+    expect(screen.getByText(book.publisher)).toBeInTheDocument()
+    expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+    expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+      'src',
+      book.coverImageUrl,
+    )
+    expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+  
+    expect(
+      screen.queryByRole('button', {name: /remove from list/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', {name: /mark as read/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', {name: /mark as unread/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('textbox', {name: /notes/i}),
+    ).not.toBeInTheDocument()
+    expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+    expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
+})
